a1de5bf6b40084251f03408589985b4c7c496cef
Cosmin Oancea <coancea76@yahoo.com>
2023-02-14 17:05:28 +0100

    minor

--- histo-L3-thrashing/kernels.cu.h
+++ histo-L3-thrashing/kernels.cu.h
@@ -42,7 +42,7 @@ multiStepKernel ( uint32_t* inp_inds
          * within the bounds of the current chunk
          * (less than UB and greater or equal to LB.)
          ************************/
-
+        int H = UB;
         if(ind < H) {
             float val = inp_vals[gid];
             atomicAdd((float*)&hist[ind], val);
